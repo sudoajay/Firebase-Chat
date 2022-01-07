775864c5feef3902a39eeae7658b5bca68820acb
SudoAjay <sudoajay@gmail.com>
2022-01-07 14:26:21 +0530

    Added Firabse Chat auth

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+
+    }
+    dependencies {
+        classpath(Plugins.pluginBuildGradle)
+        classpath(Plugins.pluginKotlinGradle)
+        classpath(Plugins.pluginDaggerHilt)
+        classpath(Plugins.googleService)
+
+
+    }
+}
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
